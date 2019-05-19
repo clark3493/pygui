0f14a09e7f8c76110a1886b08fb6a61ee1c1f5e6
clark3493 <37091734+clark3493@users.noreply.github.com>
2019-05-18 21:01:12 -0500

    Added functions for finding object instances within GUI parents/children

--- pygui/util/widget.py
+++ pygui/util/widget.py
@@ -28,3 +28,31 @@ def create_button_icon(parent, image_path, height=20, width=None, command=None, 
     if tooltip is not None:
         b.tooltip = ToolTip(b, text=tooltip)
     return b
+
+
+def find_widget_child_instance(widget, class_):
+    if isinstance(widget, class_):
+        return widget
+
+    try:
+        for child in widget.children.values():
+            if isinstance(child, class_):
+                return child
+            else:
+                return find_widget_child_instance(child, class_)
+    except AttributeError:
+        return None
+
+
+def find_widget_parent_instance(widget, class_):
+    if isinstance(widget, class_):
+        return widget
+
+    try:
+        parent = widget.parent
+        if isinstance(parent, class_):
+            return parent
+        else:
+            return find_widget_parent_instance(parent, class_)
+    except AttributeError:
+        return None
