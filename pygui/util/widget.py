ea623a0539617ce313f97d3ed1c26baa51e2f2a4
clark3493 <37091734+clark3493@users.noreply.github.com>
2019-05-19 01:38:03 -0500

    Fixed bug in search algorithm for find_widget_child_instance and find_widget_parent_instance

--- pygui/util/widget.py
+++ pygui/util/widget.py
@@ -2,14 +2,24 @@ import os
 import sys
 
 import tkinter as tk
+import warnings
 
 from PIL import Image, ImageTk
+from _tkinter import TclError
 
 SRCDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if SRCDIR not in sys.path:
     sys.path.insert(0, SRCDIR)
     
 from widget.ui import ToolTip
+
+
+def add_menu_command_safe(menu, label, command, **kwargs):
+    labels = get_menu_labels(menu)
+    if label in labels:
+        warnings.warn(f"Tried adding {label} option to menubar, but it was already defined.")
+    else:
+        menu.add_command(label=label, command=command, **kwargs)
 
 
 def create_button_icon(parent, image_path, height=20, width=None, command=None, tooltip=None):
@@ -39,7 +49,9 @@ def find_widget_child_instance(widget, class_):
             if isinstance(child, class_):
                 return child
             else:
-                return find_widget_child_instance(child, class_)
+                out = find_widget_child_instance(child, class_)
+                if out is not None:
+                    return out
     except AttributeError:
         return None
 
@@ -53,6 +65,20 @@ def find_widget_parent_instance(widget, class_):
         if isinstance(parent, class_):
             return parent
         else:
-            return find_widget_parent_instance(parent, class_)
+            out = find_widget_parent_instance(parent, class_)
+            if out is not None:
+                return out
     except AttributeError:
         return None
+
+
+def get_menu_labels(menu):
+    last = menu.index("end")
+    labels = []
+    if last is not None:
+        for index in range(last+1):
+            try:
+                labels.append(menu.entrycget(index, "label"))
+            except TclError:
+                pass
+    return labels
