7ca176225ab31a1a63eb88f9c6df87975dab9d77
clark3493 <37091734+clark3493@users.noreply.github.com>
2019-05-25 18:15:57 -0500

    Added close "x" button to tabs through a custom style

--- pygui/widget/tab_view.py
+++ pygui/widget/tab_view.py
@@ -1,13 +1,24 @@
+import tkinter as tk
+
 from tkinter import ttk
 
 
 class AbstractTabView(ttk.Notebook):
 
+    __INITIALIZED = False
     NEW_TAB_BASENAME = "new%d"
 
     def __init__(self, parent, *args, **kwargs):
+        if not self.__INITIALIZED:
+            self.__initialize_custom_style()
+            self.__INITIALIZED = True
+
+        if "style" not in kwargs:
+            kwargs["style"] = "AbstractTabView"
+
         super().__init__(parent, *args, **kwargs)
         self.parent = parent
+        self._active = None
 
         self.tab_widgets = {}
         self.bind_keys()
@@ -34,6 +45,8 @@ class AbstractTabView(ttk.Notebook):
         return new_tab
 
     def bind_keys(self):
+        self.bind("<ButtonPress-1>", self.on_left_click_press, True)
+        self.bind("<ButtonRelease-1>", self.on_left_click_release)
         self.bind('<Button-3>', self.on_right_click)
 
     def close_tab(self, index):
@@ -47,8 +60,71 @@ class AbstractTabView(ttk.Notebook):
     def get_widget(self, index, widget=''):
         return self.get_container(index).children[widget]
 
+    def on_left_click_press(self, event=None):
+        element = self.identify(event.x, event.y)
+        if "close" in element:
+            index = self.index("@%d,%d" % (event.x, event.y))
+            self.state(['pressed'])
+            self._active = index
+
+    def on_left_click_release(self, event=None):
+        if not self.instate(['pressed']):
+            return
+
+        element = self.identify(event.x, event.y)
+        index = self.index("@%d,%d" % (event.x, event.y))
+
+        if "close" in element and self._active == index:
+            self.close_tab(index)
+
+        self.state(["!pressed"])
+        self._active = None
+
     def on_right_click(self, event=None):
         pass
 
     def _add_default_tab(self):
         raise NotImplementedError
+
+    def __initialize_custom_style(self):
+        style = ttk.Style()
+        self.images = (
+            tk.PhotoImage("img_close", data='''
+            R0lGODlhCAAIAMIBAAAAADs7O4+Pj9nZ2Ts7Ozs7Ozs7Ozs7OyH+EUNyZWF0ZWQg
+            d2l0aCBHSU1QACH5BAEKAAQALAAAAAAIAAgAAAMVGDBEA0qNJyGw7AmxmuaZhWEU
+            5kEJADs=
+            '''),
+            tk.PhotoImage("img_closeactive", data='''
+            R0lGODlhCAAIAMIEAAAAAP/SAP/bNNnZ2cbGxsbGxsbGxsbGxiH5BAEKAAQALAAA
+            AAAIAAgAAAMVGDBEA0qNJyGw7AmxmuaZhWEU5kEJADs=
+            '''),
+            tk.PhotoImage("img_closepressed", data='''
+            R0lGODlhCAAIAMIEAAAAAOUqKv9mZtnZ2Ts7Ozs7Ozs7Ozs7OyH+EUNyZWF0ZWQg
+            d2l0aCBHSU1QACH5BAEKAAQALAAAAAAIAAgAAAMVGDBEA0qNJyGw7AmxmuaZhWEU
+            5kEJADs=
+            ''')
+        )
+
+        style.element_create("close", "image", "img_close",
+                             ("active", "pressed", "!disabled", "img_closepressed"),
+                             ("active", "!disabled", "img_closeactive"), border=8, sticky="")
+        style.layout("AbstractTabView", [("AbstractTabView.client", {"sticky": "nswe"})])
+        style.layout("AbstractTabView.Tab", [
+            ("AbstractTabView.tab", {"sticky": "nswe",
+                                     "children": [
+                                         ("AbstractTabView.padding", {
+                                             "side": "top",
+                                             "sticky": "nswe",
+                                             "children": [
+                                                 ("AbstractTabView.focus", {
+                                                     "side": "top",
+                                                     "sticky": "nswe",
+                                                     "children": [
+                                                         ("AbstractTabView.label", {"side": "left", "sticky": ""}),
+                                                         ("AbstractTabView.close", {"side": "left", "sticky": ""}),
+                                                     ]
+                                                 })
+                                             ]
+                                         })
+                                     ]})
+        ])
