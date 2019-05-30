334387de5fa19b46a3a980c45be3fba339d033d3
clark3493 <37091734+clark3493@users.noreply.github.com>
2019-05-29 22:24:25 -0500

    Reformatted _TabbedTextpadOptions to contain separate classes for groups of options Added hook to handle each new tab that is generated

--- pygui/widget/editor/tabbed_textpad.py
+++ pygui/widget/editor/tabbed_textpad.py
@@ -1,6 +1,7 @@
 import os
 
 import tkinter as tk
+import tkinter.font as tkfont
 
 from .textpad import TextPad
 from ..tab_view import AbstractTabView, _AbstractTabViewOptions
@@ -13,15 +14,17 @@ class TabbedTextpad(AbstractTabView):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
 
-        self.options = _TabbedTextpadOptions()
+        self.options = _TabbedTextpadOptions(self)
         self.set_options()
         self.add_tab()
         
     def add_tab(self, event=None, widget=None, text=None, **kwargs):
         if widget is None:
-            return self._add_default_tab(text=None, **kwargs)
+            name = self._add_default_tab(text=None, **kwargs)
         else:
-            return super().add_tab(widget=widget, text=text)
+            name = super().add_tab(widget=widget, text=text)
+        self._handle_new_tab(name)
+        return name
         
     def bind_keys(self):
         super().bind_keys()
@@ -53,6 +56,12 @@ class TabbedTextpad(AbstractTabView):
         pad = self.get_widget(index)
         path = pad.functions['save_file_as']()
         self.tab(self.tabs()[index], text=os.path.split(path)[-1])
+
+    def set_editor_tab_spacing(self, value):
+        for name, widget in self.tab_widgets.items():
+            if '!textpad' in widget.children:
+                pad = widget.children['!textpad']
+                pad.set_tab_spacing(value)
             
     def set_options(self):
         self.option_add('*tearOff', False)
@@ -68,13 +77,36 @@ class TabbedTextpad(AbstractTabView):
 
         return new_tab
 
+    def _handle_new_tab(self, name):
+        widget = self.tab_widgets[name]
+
+        if '!textpad' in widget.children:
+            pad = widget.children['!textpad']
+            pad.set_tab_spacing(self.options.text_entry_options.tab_spacing)
+
 
 class _TabbedTextpadOptions(_AbstractTabViewOptions):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.text_entry_options = {'tab spacing': 4}
+        self.text_entry_options = _TextEntryOptions(self)
+
+
+class _TextEntryOptions(object):
+
+    def __init__(self, parent):
+        self.parent = parent
+        self._tab_spacing = 4
+
+    @property
+    def tab_spacing(self):
+        return self._tab_spacing
+
+    @tab_spacing.setter
+    def tab_spacing(self, value):
+        self._tab_spacing = value
+        self.parent.parent.set_editor_tab_spacing(value)
             
             
 class _TextpadTabPopup(tk.Menu):
