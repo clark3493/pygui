99211e6b25a1b89f983a402f13e1191c2ffc354d
clark3493 <37091734+clark3493@users.noreply.github.com>
2019-06-04 22:21:12 -0500

    Updated tests for Run filtering

--- test/data/test_run.py
+++ test/data/test_run.py
@@ -20,6 +20,84 @@ class RunDataTestCase(unittest.TestCase):
     def assertAllClose(self, a, b):
         self.assertTrue(np.allclose(a, b))
 
+    @staticmethod
+    def simple_filter(run, column, inc):
+        """Add increment 'inc' to run[column]"""
+        run.loc[:, column] = run.loc[:, column] + inc
+
+    def test_filter_no_errors(self):
+        run = Run.read_csv(self.TEST_DATA_FILEPATH)
+        run.filter_data(self.simple_filter, 'A', 3)
+
+    def test_filter_set_is_filtered_attribute(self):
+        run = Run.read_csv(self.TEST_DATA_FILEPATH)
+        run.filter_data(self.simple_filter, 'A', 3)
+        self.assertTrue(run.is_filtered)
+
+    def test_filter_get_stored_filter(self):
+        run = Run.read_csv(self.TEST_DATA_FILEPATH)
+        run.filter_data(self.simple_filter, 'A', 3)
+        f = run.filters.funcs['simple_filter']
+        self.assertTrue(f is self.simple_filter)
+
+    def test_filter_getitem_stored_filter(self):
+        run = Run.read_csv(self.TEST_DATA_FILEPATH)
+        run.filter_data(self.simple_filter, 'A', 3)
+        f = run.filters['simple_filter']
+        self.assertTrue(f is self.simple_filter)
+
+    def test_filter_get_stored_args(self):
+        run = Run.read_csv(self.TEST_DATA_FILEPATH)
+        run.filter_data(self.simple_filter, 'A', 3)
+        args = run.filters.args['simple_filter']
+        self.assertEqual(args, ('A', 3))
+
+    def test_filter_modifies_run_in_place(self):
+        run = Run.read_csv(self.TEST_DATA_FILEPATH)
+        orig_values = run['A'].tolist()
+        run.filter_data(self.simple_filter, 'A', 3)
+        new_values = run['A'].tolist()
+        self.assertTrue(all(x-3. == y for x, y in zip(new_values, orig_values)))
+
+    def test_filter_clear_filter_collector(self):
+        run = Run.read_csv(self.TEST_DATA_FILEPATH)
+        orig_values = run['A'].tolist()
+        run.filter_data(self.simple_filter, 'A', 3)
+        self.assertNotAlmostEqual(orig_values[0], run['A'][0])  # make sure run changed
+        run.filters.clear()
+        self.assertTrue(run.filters.funcs == {})
+        self.assertTrue(run.filters.args == {})
+        self.assertTrue(run.filters.kwargs == {})
+
+    def test_filter_unfilter(self):
+        run = Run.read_csv(self.TEST_DATA_FILEPATH)
+        orig_values = run['A'].tolist()
+        run.filter_data(self.simple_filter, 'A', 3)
+        self.assertNotAlmostEqual(orig_values[0], run['A'][0])
+        run.unfilter()
+        new_values = run['A'].tolist()
+        self.assertTrue(x == y for x, y in zip(orig_values, new_values))
+
+    def test_filter_unfilter_removes_added_data(self):
+        def add_column(df, column, inc, new_column='Z'):
+            df.loc[:, column] = df.loc[:, column] + inc
+            df[new_column] = 99
+            df.loc[100] = [1., 2., 3., 4., 5., 6., 7.]
+        run = Run.read_csv(self.TEST_DATA_FILEPATH)
+        run.filter_data(add_column, 'A', 3)
+        self.assertTrue(len(run['Z']) == len(run['B']))
+        self.assertTrue(100 in run.index)
+        run.unfilter()
+        self.assertTrue('Z' not in run.columns)
+        self.assertTrue(100 not in run.index)
+
+    def test_filter_unfilter_removes_copy_reference(self):
+        run = Run.read_csv(self.TEST_DATA_FILEPATH)
+        run.filter_data(self.simple_filter, 'A', 3)
+        self.assertIsNotNone(run._original_run)
+        run.unfilter()
+        self.assertIsNone(run._original_run)
+
     def test_read_csv_returns_dataframe_instance(self):
         run = Run.read_csv(self.TEST_DATA_FILEPATH)
         self.assertTrue(isinstance(run, pd.DataFrame))
